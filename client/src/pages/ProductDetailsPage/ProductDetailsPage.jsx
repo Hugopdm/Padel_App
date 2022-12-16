e9d13955b100396010bcd389c32aa14525ffe20a
Hugo Paniagua <hugopandm@gmail.com>
2022-12-16 09:05:46 +0100

    last review

--- client/src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ client/src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,3 +1,5 @@
+import './ProductDetailsPage.css'
+
 import { useEffect, useState } from "react"
 import { Container, Row, Col, Card, Button } from "react-bootstrap"
 import { Link, useParams } from "react-router-dom"
@@ -37,10 +39,10 @@ const ProductDetailsPage = () => {
                             <h1 className="text-center">{category}</h1>
                             <hr />
                             <Link className="mt-4 text-center" to="/productos">
-                                <Button as="div" variant="outline-dark">Volver a la galería</Button>
+                                <Button as="div" variant="outline-dark" className='products'>Volver a la galería</Button>
                             </Link>
 
-                            <Col className="mt-5" md={{ span: 3 }}>
+                            <Col className="img mt-5" md={{ span: 3 }}>
                                 <img src={imageUrl} style={{ width: '100%' }} />
                             </Col>
 
@@ -50,7 +52,7 @@ const ProductDetailsPage = () => {
                                     <Card.Body>
                                         <Card.Title>{productName}</Card.Title>
                                         <Card.Text>{description}</Card.Text>
-                                        <Card.Text>{price} € </Card.Text>
+                                        <Card.Text className="price">{price} € </Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Col>
